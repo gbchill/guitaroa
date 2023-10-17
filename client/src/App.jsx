bc36b4e68b61f029c8956433da1befe2353e3db9
George Badulescu <georgebadulescu123@gmail.com>
2023-10-16 19:01:08 -0700

    login and register page

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+import RegisterPage from './pages/RegisterPage';
+import axios from 'axios';
+import { UserContextProvider } from './UserContext';
+
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          {/* Creates route for different locations in the app */}
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  )
+}
+
+export default App
